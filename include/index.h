8ab0441b9b46d67bd3725227f46151d4b9d5568e
proc0 <alanvx@gmail.com>
2026-09-13 02:41:05 -0700

    CMake: small formatting

--- include/index.h
+++ include/index.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define PROJECT_NAME "rayclay"
-#define VERSION "0.2.0"
+#define VERSION "0.3.0"
 #define DEBUG 1
-#define DATE_BUILD "2026-09-12 20:16:59"
+#define DATE_BUILD "2026-09-13 02:40:26"
 #define DIR_ASSETS "asset"
 
 #define PATH_ASSET(filename) DIR_ASSETS "/" filename
